36cfe326fb3475d1d2effd042649e4d4eadfa298
git config --global user.name <isaiposada@gmail.com>
2022-08-04 18:08:06 -0400

    homework completed

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,12 +1,37 @@
 'use strict'
 
-function BinarioADecimal(num) {
-  // tu codigo aca
+//const { TestWatcher } = require("jest");
 
+
+function BinarioADecimal(num) {
+
+  let binaryStr = num.toString(), arra = [], result = 0;
+  
+  for(var i=0; i<binaryStr.length; i++){
+      arra.unshift(Math.pow(2,i));
+  }
+  
+  for(var j=0; j<binaryStr.length; j++){
+  
+      if(binaryStr.charAt(j) === "1"){
+          result += arra[j];
+      }
+  }
+  
+  return result;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
+ 
+  let residuo = [];
+
+  while(num > 0){
+
+       residuo.unshift(num%2);
+
+       num = Math.floor(num/2);
+   }
+   return  residuo.join('');
 
 }
 
